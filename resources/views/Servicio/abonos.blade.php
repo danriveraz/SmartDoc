d41081c93d648df1e44d25f80bc7bc759d15955b
Felipe Castaño <castano.luis@correounivalle.edu.co>
2018-08-14 17:59:31 -0500

    Correccion chida commit anterior, la anterior tambien es chida

--- resources/views/Servicio/abonos.blade.php
+++ resources/views/Servicio/abonos.blade.php
@@ -223,17 +223,6 @@
 	};
 
 	function imprimr() {
-		swal({
-        title: "Seguro que quieres hacer esto?",
-          text: "Esta acción ya no se podrá deshacer, Así que piénsalo bien.",
-          type: "warning",
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: 'Si, estoy seguro',
-          cancelButtonText: "Cancelar"
-        });
-        
 		document.getElementsByClassName('page-header-fixed')[0].style.paddingTop = '20px';
 		document.getElementsByClassName('card-body')[0].style.borderBottom = 0;
 		document.getElementsByClassName('card-body')[0].style.paddingBottom = 0;
